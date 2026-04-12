2ef5b02a6dfab2bc6abcf3851d3ae0ceba05b358
sankha091 <sankha091@gmail.com>
2026-04-13 02:02:20 +0530

    Data Transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,7 +2,11 @@ import sys
 import os
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -20,14 +24,20 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv('notebook/data/stud.csv')
+
             logging.info('Read the dataset as dataframe')
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False)
+
             logging.info('Train test split initiated')
+
             train_set,test_set = train_test_split(df, test_size = 0.2, random_state = 42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
             logging.info("Ingestion of the data is completed")
+            
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
@@ -39,7 +49,11 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
 #---- IGNORE THIS ----
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,6 +3,7 @@ import os
 from src.exception import CustomException
 from src.logger import logging
 from dataclasses import dataclass
+from src.utils import save_object
 
 import numpy as np
 import pandas as pd
@@ -18,7 +19,8 @@ class DataTransformationConfig:
 
 
 class DataTransformation:
-    # The DataTransformation class is responsible for handling the data transformation process, which includes creating a preprocessor object and applying it to the training and testing datasets.
+    # The DataTransformation class is responsible for handling the data transformation process, 
+    # which includes creating a preprocessor object and applying it to the training and testing datasets.
 
     def __init__(self):
         # __init_ method is used to initialize the DataTransformationConfig dataclass and assign it to an instance variable.
@@ -50,7 +52,7 @@ class DataTransformation:
                 steps = [
                     ("imputer", SimpleImputer(strategy = "most_frequent")),
                     ("one_hot_emcoder", OneHotEncoder()),
-                    ('scaler', StandardScaler())
+                    ('scaler', StandardScaler(with_mean=False))
                 ]
             )
 
@@ -75,8 +77,44 @@ class DataTransformation:
     def initiate_data_transformation(self, train_path, test_path):
         # This method is responsible for reading the training and testing data, applying the preprocessor object to transform the data, and saving the preprocessor object for future use.
         try:
-            pass
-        
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessor object")
+
+            preprocessing_obj = self.get_data_transformaer_object()
+
+            target_column_name = "math_score"
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("Applying preprocessing object on training and testing dataframes")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr =  preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("Saved preprocessing object")
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+
+
+
         except Exception as e:
             logging.info("Error occurred in Data Transformation initiate_data_transformation")
             raise CustomException(e, sys)
